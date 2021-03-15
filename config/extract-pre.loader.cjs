9a9bbd0286ebc14d6eba59d17aedfb2a33f60880
kongxiangyan <kcigaret@outlook.com>
2021-03-16 02:13:37 +0800

    chore: test css-loader@5

--- config/extract-pre.loader.cjs
+++ config/extract-pre.loader.cjs
@@ -1,11 +1,16 @@
 
 // export default function main (source) {
-//   return source.replace(/require\("(.*)"\);/g, 'require("css-loader/dist/runtime/api.js")')
+//   return source
+//     .replace(/from "(.*)cssWithMappingToString.js";/g, 'from "css-loader/dist/runtime/cssWithMappingToString.js";')
+//     .replace(/from "(.*)api.js";/g, 'from "css-loader/dist/runtime/api.js";')
 // }
 
 module.exports = function (source) {
   const newSource = source.replace(/require\("(.*)"\);/g, 'require("css-loader/dist/runtime/api.js")')
-
+  // ! css-loader@5 使用 es6 模块，暂时不能正常工作
+  // const newSource = source
+  //   .replace(/from "(.*)cssWithMappingToString.js";/g, 'from "css-loader/dist/runtime/cssWithMappingToString.js";')
+  //   .replace(/from "(.*)api.js";/g, 'from "css-loader/dist/runtime/api.js";')
   // ! hot reload 太麻烦了
 
   // let css
