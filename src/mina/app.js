0922b59d2726011abafc3836740ae89eddc80c59
kongxiangyan <kcigaret@outlook.com>
2021-03-17 14:53:16 +0800

    WIP: simplify app.js

--- src/mina/app.js
+++ src/mina/app.js
@@ -1,30 +1,12 @@
-/* eslint-disable no-undef */
 import { replayWithLatest, Data } from 'MobiusUtils'
-import { html, makeComponentWithReplay } from 'MobiusUI'
 
-const author = replayWithLatest(1, Data.of('cigaret'))
+const whisperRD = replayWithLatest(1, Data.of('The owner is looking for a job as a product manager | business manager.\n\nFor a quickest preview of his info, check https://www.cigaret.world'))
 
-// app.js
 App({
-  onLaunch (options) {
-    console.log('[app lunch]')
-    author.subscribe(({ value }) => {
-      console.warn(value)
+  onLaunch(options) {
+    console.log('[app launch]')
+    whisperRD.subscribe(({ value: whisper }) => {
+      console.log(whisper)
     })
-    // Do something initial when launch.ggg
-  },
-  onShow (options) {
-    // Do something when show.
-  },
-  onHide () {
-    // Do something when hide.
-  },
-  onError (msg) {
-    console.log(msg)
-  },
-  globalData: 'I am global data!'
+  }
 })
-
-// xxx.js
-const appInstance = getApp()
-console.log(appInstance.globalData) // I am global data
